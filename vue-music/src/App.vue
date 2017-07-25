bd2efee152c22f30b8bbc35ab88d219853ed5663
weishuangjian <1607074855@qq.com>
2017-07-25 16:27:26 +0800

    vue-cli 搭建环境
    更改默认目录结构，搭建开发环境

---
+++ vue-music/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  @import url("common/less/variable");
+
+  #app {
+    color: @color-theme;
+  }
+</style>
---
+++ vue-music/src/main.js
@@ -0,0 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+// import router from './router'
+
+import 'common/less/index.less';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
