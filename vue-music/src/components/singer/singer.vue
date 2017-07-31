3c89d2252e92cfa6ba0879f1785d9ec24b9180b2
weishuangjian <1607074855@qq.com>
2017-07-31 17:58:23 +0800

    listview组件的基础布局和样式

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -1,13 +1,96 @@
 <template>
   <div class="singer">
-    singer
+    <v-listview :data="singers"></v-listview>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  import {getSingerList} from 'api/singer';
+  import {ERR_OK} from 'api/config';
+  import Singer from 'common/js/singer';
+  import Listview from 'base/listview/listview';
+
+  const HOT_NAME = '热门';
+  const HOT_SINGER_LEN = 10;
+
+  export default {
+    data() {
+      return {
+        singers: []
+      };
+    },
+    components: {
+      'v-listview': Listview
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list);
+          }
+        });
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        };
+
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }));
+          }
+
+          const key = item.Findex;
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }));
+        });
+
+        // 为了得到有序列表 处理map
+        let hot = [];
+        let ret = [];
+
+        for (let key in map) {
+          let val = map[key];
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val);
+          } else if (val.title === HOT_NAME) {
+            hot.push(val);
+          }
+        }
+
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        });
+
+        return hot.concat(ret);
+      }
+    }
+  };
 </script>
 
 <style rel="stylesheet/less" lang="less">
-
+  .singer {
+    position: fixed;
+    top: 80px;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
